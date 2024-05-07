db73961fe9250b0c679bde2568f143f30bce5631
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-05-07 12:09:41 -0700

    add boilerplate

---
+++ decrypt.py
@@ -0,0 +1,14 @@
+import os
+
+class Decrypt:
+    def __init__(self):
+        self._secret_filename = "secret.txt"
+
+    def getKey(self):
+        pass
+
+    def getText(self):
+        pass
+
+    def decipherMessage(self):
+        pass
---
+++ decrypt_demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from decrypt import Decrypt
+
+def main():
+    """ 
+        main method to test decrypt class
+        this module runs as a script
+    """
+    print(f'the script ran!')
+
+if __name__ == '__main__':
+    main()
